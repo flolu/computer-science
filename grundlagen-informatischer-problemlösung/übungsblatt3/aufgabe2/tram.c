47cda8dd8e0920d942949844d6cce8a4fcfcdc70
flolude <fllu@pm.me>
2019-11-11 07:03:41 +0100

    complete gip ü3 a2

--- grundlagen-informatischer-problemlösung/übungsblatt3/aufgabe2/tram.c
+++ grundlagen-informatischer-problemlösung/übungsblatt3/aufgabe2/tram.c
@@ -2,77 +2,96 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-struct station {
-  short unsigned int line;
-  short unsigned int stop;
+struct station
+{
+  int line;
+  int stop;
 };
 
-struct trip {
+struct trip
+{
   struct station start;
   struct station end;
 };
 
-void validate_input(struct trip trip) {
-
+bool validate_input(struct trip trip, int MIN, int MAX)
+{
+  if (trip.start.line > MAX || trip.start.stop > MAX || trip.end.line > MAX || trip.end.stop > MAX ||
+      trip.start.line < MIN || trip.start.stop < MIN || trip.end.line < MIN || trip.end.stop < MIN)
+    return false;
+  return true;
 }
 
-bool is_short_trip(struct trip trip) {
-  if ((trip.start.line == 5 && trip.start.stop == 2) && (trip.end.line == 4 && trip.end.stop == 2)) {
+bool is_short_trip(struct trip trip)
+{
+  if ((trip.start.line == 5 && trip.start.stop == 2) && (trip.end.line == 4 && trip.end.stop == 2) ||
+      (trip.start.line == 4 && trip.start.stop == 2) && (trip.end.line == 5 && trip.end.stop == 2))
     return false;
-  }
-  if ((trip.start.line == 4 && trip.start.stop == 2) && (trip.end.line == 5 && trip.end.stop == 2)) {
-    return false;
-  }
 
-  if ((trip.start.line == 0 && trip.start.stop == 0) && (trip.end.line == 2 && trip.end.stop == 1)) {
+  if ((trip.start.line == 0 && trip.start.stop == 0) && (trip.end.line == 2 && trip.end.stop == 1) ||
+      (trip.start.line == 2 && trip.start.stop == 1) && (trip.end.line == 0 && trip.end.stop == 0))
     return false;
-  }
-  if ((trip.start.line == 2 && trip.start.stop == 1) && (trip.end.line == 0 && trip.end.stop == 0)) {
-    return false;
-  }
 
-  if (trip.start.line == trip.end.line && abs(trip.start.stop - trip.end.stop) == 1) {
-    return true;
+  int stop_difference = abs(trip.start.stop - trip.end.stop);
+  if (stop_difference == 1)
+  {
+    if (trip.start.line == trip.end.line)
+      return true;
+    if (trip.start.stop == 0 && trip.end.stop == 1 ||
+        trip.start.stop == 1 && trip.end.stop == 0)
+      return true;
   }
 
   return false;
 }
 
-int count_zone_crosses(struct trip trip) {
+int count_zone_crosses(struct trip trip)
+{
+  if (trip.start.stop > 2 && trip.end.stop > 2 && trip.start.line != trip.end.line)
+    return 2;
+  if (trip.start.stop > 2 && trip.end.stop < 3)
+    return 1;
+  if (trip.start.stop < 3 && trip.end.stop > 2)
+    return 1;
   return 0;
 }
 
-int count_end_stops(struct trip trip) {
-  return 0;
+int count_end_stops(struct trip trip)
+{
+  int end_stops = 0;
+  if (trip.start.stop == 5)
+    end_stops++;
+  if (trip.end.stop == 5)
+    end_stops++;
+  return end_stops;
 }
 
-int main() {
-  int start = 00, end = 00;
+int main()
+{
+  int start_input, end_input;
+  const int DEFAULT_PRICE = 3,
+            SHORT_TRIP_PRICE = 2,
+            END_STOP_PRICE = 1,
+            ZONE_CROSS_PRICE = 1;
 
   printf("Starthaltestelle: ");
-  scanf("%d", &start);
+  scanf("%d", &start_input);
   printf("Zielhaltestelle: ");
-  scanf("%d", &end);
+  scanf("%d", &end_input);
 
-  struct station start_station = { (start - (start % 10)) / 10, start % 10 };
-  struct station end_station = { (end - (end % 10)) / 10, end % 10 };
-  struct trip trip = { start_station, end_station };
-
-  validate_input(trip);
-
-  int price = 3;
-
-  if (is_short_trip(trip)) {
-    price = 2;
+  struct station start_station = {(start_input / 10) % 10, start_input % 10};
+  struct station end_station = {(end_input / 10) % 10, end_input % 10};
+  struct trip trip = {start_station, end_station};
+  if (!validate_input(trip, 0, 5))
+  {
+    printf("Invalid input\n");
+    return 1;
   }
 
-  int zone_crosses = count_zone_crosses(trip);
-  price += zone_crosses;
+  int price = is_short_trip(trip) ? SHORT_TRIP_PRICE : DEFAULT_PRICE;
+  price += count_zone_crosses(trip) * ZONE_CROSS_PRICE;
+  price += count_end_stops(trip) * END_STOP_PRICE;
 
-  int end_stops = count_end_stops(trip);
-  price += end_stops;
-
-  printf("is_short_trip: %d, zone_crosses: %d, end_stops: %d\n", is_short_trip(trip), zone_crosses, end_stops);
   printf("%d Euro\n", price);
   return 0;
 }
