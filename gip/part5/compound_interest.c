956eced21b9ff0b426a2e8eebafc1f282ef6d3b8
flolude <fllu@pm.me>
2019-11-26 21:11:19 +0100

    gip part5 print table

--- gip/part5/compound_interest.c
+++ gip/part5/compound_interest.c
@@ -8,9 +8,13 @@ int main(void) {
   printf("Zinssatz in Prozent: "); scanf("%lf", &interest_rate);
   printf("Laufzeit in Jahren: "); scanf("%d", &years);
 
+  printf("┏━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━┓\n");
+  printf("┃ Jahr\t┃ Kapital in EUR\t┃\n");
+  printf("┣━━━━━━━╋━━━━━━━━━━━━━━━━━━━━━━━┫\n");
   for (int i = 1; i <= years; i++) {
     double current_capital = capital * pow((1 + (interest_rate / 100)), i);
-    printf("Kapital im %d.ten Jahr: %.2lf EUR\n", i, current_capital);
+    printf("┃ %d\t┃ %.2lf\t\t┃\n", i, current_capital);
   }
+  printf("┗━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━┛\n");
   return 0;
 }
