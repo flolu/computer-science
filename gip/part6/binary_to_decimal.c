5f0ece5cfb1a4918993da3f4410ee7677e18c8b7
flolude <fllu@pm.me>
2019-11-29 21:02:51 +0100

    gip part6

---
+++ gip/part6/binary_to_decimal.c
@@ -0,0 +1,31 @@
+#include<math.h>
+#include<stdio.h>
+
+int binaryToDecimal(long int binary) {
+  int decimal = 0, exponent = 0;
+  while(binary > 0) {
+    decimal += binary % 10 == 1 ? pow(2, exponent) : 0;
+    binary /= 10;
+    exponent++;
+  }
+  return decimal;
+}
+
+long int decimalToBinary(int decimal) {
+  long int binary = 0, i = 1;
+  while (decimal > 0) {
+    int remainder = decimal % 2;
+    decimal /= 2;
+    binary = binary + remainder * i;
+    i *= 10;
+  }
+  return binary;
+}
+
+int main() {
+  long int binary = 1000011101010011;
+  printf("%ld as decimal is: %d\n", binary, binaryToDecimal(binary));
+  int decimal= 34643;
+  printf("%d as binary is: %ld\n", decimal, decimalToBinary(decimal));
+  return 0;
+}
---
+++ gip/part6/solve_quadratic_function.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+
+int solve(int a, int b, int c, double* x1, double* x2) {
+  double inside_root = b * b - 4 * a * c;
+  *x1 = (-b + sqrt(inside_root)) / (2 * a);
+  *x2 = (-b - sqrt(inside_root)) / (2 * a);
+  if (inside_root > 0)
+    return 2;
+  if (inside_root == 0)
+    return 1;
+  if (inside_root < 0)
+    return 0;
+}
+
+int main() {
+  double a, b, c;
+  scanf("%lf %lf %lf", &a, &b, &c);
+  double x1, x2;
+  int anzahl = solve(a, b, c, &x1, &x2);
+  if (!anzahl)
+    printf("keine Lösung");
+  else if (anzahl == 1)
+    printf("eine Lösung x: %lf", x1);
+  else if (anzahl > 1)
+    printf("zwei Lösungen x1: %lf x2: %lf", x1, x2);
+}
